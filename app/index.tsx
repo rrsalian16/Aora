eb2d840909c276ba6a016a23016f35dc0c326f7e
rrsalian16 <rrsalian16@gmail.com>
2024-07-13 15:44:19 +0400

    feat: add route layouts

--- app/index.tsx
+++ app/index.tsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View className="flex-1 justify-center content-center items-center ">
       <Text className="text-3xl font-pblack">Hello Aora</Text>
-      <Link href="/profile" style={{ color: 'blue' }}>
+      <Link href="/home" style={{ color: 'blue' }}>
         Go to Profile
       </Link>
     </View>
