2b488cbd5b14d0a45cf9d42ef5028cde2ab2b687
rrsalian16 <rrsalian16@gmail.com>
2024-07-13 15:36:29 +0400

    feat: add tailwind font and assets

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,14 @@
+import { Link } from 'expo-router';
 import React from 'react';
 import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello Aora</Text>
+    <View className="flex-1 justify-center content-center items-center ">
+      <Text className="text-3xl font-pblack">Hello Aora</Text>
+      <Link href="/profile" style={{ color: 'blue' }}>
+        Go to Profile
+      </Link>
     </View>
   );
 }
